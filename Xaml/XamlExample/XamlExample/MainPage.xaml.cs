25805793e436c6ece485bc2f9baebda5eeafcd3c
Christopher Bennage <christopher@bennage.com>
2010-09-29 10:14:30 -0400

    renamed episode directories

---
+++ Xaml/XamlExample/XamlExample/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace XamlExample
+{
+	using System.Windows.Controls;
+	using System.Windows.Media;
+	using Microsoft.Phone.Controls;
+
+	public partial class MainPage : PhoneApplicationPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		void CreateButton()
+		{
+			var button = new Button
+			             	{
+			             		Content = "Click Me",
+			             		FontSize = 26,
+			             		Height = 60,
+			             		Width = 200,
+								//Background = new SolidColorBrush(Colors.Green)
+			             	};
+
+			LayoutRoot.Children.Add(button);
+		}
+	}
+}
