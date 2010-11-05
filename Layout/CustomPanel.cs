28c47b5172bd4d29a14aa4c5ab98b7cfeb9926e5
Christopher Bennage <christopher@bennage.com>
2010-11-05 15:10:54 -0400

    source for episode 5 on layout

---
+++ Layout/CustomPanel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Layout
+{
+	public class CustomPanel : Panel
+	{
+		protected override Size MeasureOverride(Size availableSize)
+		{
+			return base.MeasureOverride(availableSize);
+		}
+
+		protected override Size ArrangeOverride(Size finalSize)
+		{
+			return base.ArrangeOverride(finalSize);
+		}
+
+	}
+}
